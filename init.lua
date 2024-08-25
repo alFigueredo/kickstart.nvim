591a046dd7f8fda2c2ebf2a1a687faa78c723baa
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2024-08-25 04:09:03 +0000

    Expandtab

--- init.lua
+++ init.lua
@@ -108,7 +108,7 @@ vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
