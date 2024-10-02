5c7c6eb97cd655949e330c63c69d8620f453eb24
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2024-10-02 20:24:12 +0000

    set relative numbers

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Tab and indent config
 vim.opt.tabstop = 4
