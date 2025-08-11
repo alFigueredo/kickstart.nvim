3370c63c865bc083540b88f93a41b5c4249606d7
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-08-11 01:11:34 -0300

    Fix: jdtls lsp attaching twice

--- lua/panki/lazy/lsp.lua
+++ lua/panki/lazy/lsp.lua
@@ -258,6 +258,9 @@ return {
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
+        automatic_enable = {
+          exclude = { 'jdtls' },
+        },
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
