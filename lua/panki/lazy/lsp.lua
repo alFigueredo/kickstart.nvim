f42643f1155a4de64445cd412f7a7baea88a9296
Alan Lionel Figueredo Aguiar <lionelfigueredo@gmail.com>
2025-11-25 19:33:06 -0300

    Fix: jdtls enabled config

--- lua/panki/lazy/lsp.lua
+++ lua/panki/lazy/lsp.lua
@@ -258,9 +258,6 @@ return {
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
-        automatic_enable = {
-          'jdtls',
-        },
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
